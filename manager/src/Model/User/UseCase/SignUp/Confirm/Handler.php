e75cb374c92062693ca9bad542e06bce88bfd1fc
rupa4ok <rupak4@yandex.ru>
2019-08-29 16:50:38 +0300

    Add reset tokenenizer

--- manager/src/Model/User/UseCase/SignUp/Confirm/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Confirm/Handler.php
@@ -5,11 +5,7 @@ declare(strict_types=1);
 namespace App\Model\User\UseCase\SignUp\Confirm;
 
 use App\Model\Flusher;
-use App\Model\User\Entity\Email;
-use App\Model\User\Entity\Id;
-use App\Model\User\Entity\User;
 use App\Model\User\Entity\UserRepository;
-use App\Model\User\Service\PasswordHasher;
 
 class Handler
 {
