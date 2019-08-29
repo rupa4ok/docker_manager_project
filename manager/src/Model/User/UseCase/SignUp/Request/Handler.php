8d65cbb79269e9d14696ae03708cb6a856fa25fb
rupa4ok <rupak4@yandex.ru>
2019-08-29 10:44:19 +0300

    New confirm email tokenizer

--- manager/src/Model/User/UseCase/SignUp/Request/Handler.php
+++ manager/src/Model/User/UseCase/SignUp/Request/Handler.php
@@ -10,21 +10,28 @@ use App\Model\User\Entity\Id;
 use App\Model\User\Entity\User;
 use App\Model\User\Entity\UserRepository;
 use App\Model\User\Service\PasswordHasher;
+use App\Model\User\Service\SignUpConfirmTokenizer;
+use App\Model\User\Service\SignUpConfirmTokenSender;
 
 class Handler
 {
     private $users;
     private $hasher;
+    private $tokenizer;
+    private $sender;
     private $flusher;
-
     public function __construct(
         UserRepository $users,
         PasswordHasher $hasher,
+        SignUpConfirmTokenizer $tokenizer,
+        SignUpConfirmTokenSender $sender,
         Flusher $flusher
     )
     {
         $this->users = $users;
         $this->hasher = $hasher;
+        $this->tokenizer = $tokenizer;
+        $this->sender = $sender;
         $this->flusher = $flusher;
     }
 
@@ -40,7 +47,8 @@ class Handler
             Id::next(),
             new \DateTimeImmutable(),
             $email,
-            $this->hasher->hash($command->password)
+            $this->hasher->hash($command->password),
+            $token = $this->tokenizer->generate()
         );
 
         $this->users->add($user);
