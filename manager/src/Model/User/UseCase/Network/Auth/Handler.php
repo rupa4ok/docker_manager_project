0d621d71f2f09115294a74dba352b1e93e7138b8
rupa4ok <rupak4@yandex.ru>
2019-08-29 10:41:12 +0300

    Add UseCase network autification

---
+++ manager/src/Model/User/UseCase/Network/Auth/Handler.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Network\Auth;
+
+use App\Model\Flusher;
+use App\Model\User\Entity\Email;
+use App\Model\User\Entity\Id;
+use App\Model\User\Entity\User;
+use App\Model\User\Entity\UserRepository;
+use App\Model\User\Service\PasswordHasher;
+
+class Handler
+{
+    private $users;
+    private $flusher;
+
+    public function __construct(
+        UserRepository $users,
+        Flusher $flusher
+    )
+    {
+        $this->users = $users;
+        $this->flusher = $flusher;
+    }
+
+    public function handle(Command $command): void
+    {
+        if ($user = $this->users->hasByNetworkIdentity($command->network, $command->identity)) {
+            throw new \DomainException('Пользователь уже существует');
+        }
+
+        $user = User::signUpByNetwork(
+            Id::next(),
+            new \DateTimeImmutable(),
+            $command->network,
+            $command->identity
+        );
+
+        $this->users->add($user);
+
+        $this->flusher->flush();
+    }
+}
