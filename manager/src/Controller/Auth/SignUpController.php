1e2b2cbc2c1404697df276f24170c30685f19824
Rupa4ok <rupak4@yandex.ru>
2019-09-03 08:12:11 +0300

    Add signup template

---
+++ manager/src/Controller/Auth/SignUpController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Auth;
+
+use App\Model\User\UseCase\SignUp;
+use App\Security\LoginFormAuthenticator;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+class SignUpController extends AbstractController
+{
+	private $logger;
+	
+	public function __construct(LoggerInterface $logger)
+	{
+		$this->logger = $logger;
+	}
+	
+	/**
+	 * @Route("/signup", name="auth.signup")
+	 * @param Request $request
+	 * @param SignUp\Request\Handler $handler
+	 * @return Response
+	 */
+	public function request(Request $request, SignUp\Request\Handler $handler): Response
+	{
+		$command = new SignUp\Request\Command();
+		
+		$form = $this->createForm(SignUp\Request\Form::class, $command);
+		$form->handleRequest($request);
+		
+		if ($form->isSubmitted() && $form->isValid()) {
+			try {
+				$handler->handle($command);
+				$this->addFlash('succes', 'Проверьте вашу почту');
+				return $this->redirectToRoute('/');
+			} catch (\DomainException $e) {
+				$this->logger->error($e->getMessage(), ['exception' => $e]);
+				$this->addFlash('error', $e->getMessage());
+			}
+		}
+		
+		return $this->render('app/auth/signup.html.twig', [
+			'form' => $form->createView()
+		]);
+	}
+}
--- manager/src/Model/User/Entity/UserRepository.php
+++ manager/src/Model/User/Entity/UserRepository.php
@@ -4,19 +4,89 @@ declare(strict_types=1);
 
 namespace App\Model\User\Entity;
 
-interface UserRepository
+use App\Model\EntityNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository
 {
-    public function hasByEmail(Email $email): bool;
-
-    public function add(User $user): void;
-
-    public function hasByNetworkIdentity(string $network, string $identity): bool;
-
-    public function getByEmail(Email $email): User;
-
-    public function findByConfirmToken($token): ?User;
-
-    public function findByResetToken($token): User;
+	private $em;
+	/**
+	 * @var EntityRepository
+	 */
+	private $repo;
 	
-	public function get(Id $id): User;
+	public function __construct(EntityManagerInterface $em)
+	{
+		$this->em = $em;
+		$this->repo = $em->getRepository(User::class);
+	}
+	
+	/**
+	 * @param string $token
+	 * @return User|null|object
+	 */
+	public function findByConfirmToken(string $token): ?User
+	{
+		return $this->repo->findOneBy(['confirmToken' => $token]);
+	}
+	
+	/**
+	 * @param string $token
+	 * @return User|null|object
+	 */
+	public function findByResetToken(string $token): ?User
+	{
+		return $this->repo->findOneBy(['resetToken.token' => $token]);
+	}
+	
+	public function get(Id $id): User
+	{
+		/**
+		 * @var User $user
+		 */
+		if (!$user = $this->repo->find($id->getValue())) {
+			throw new EntityNotFoundException('Пользователь не найден');
+		}
+		return $user;
+	}
+	
+	public function getByEmail(Email $email): User
+	{
+		/**
+		 * @var User $user
+		 */
+		if (!$user = $this->repo->findOneBy(['email' => $email->getValue()])) {
+			throw new EntityNotFoundException('Пользователь не найден');
+		}
+		return $user;
+	}
+	
+	public function hasByEmail(Email $email): bool
+	{
+		return $this->repo->createQueryBuilder('t')
+			->select('COUNT(t.id)')
+			->andWhere('t.email = :email')
+			->setParameter(':email', $email->getValue())
+			->getQuery()
+			->getSingleResult() > 0;
+	}
+	
+	public function hasByNetworkIdentity(string $network, string $identity): bool
+	{
+		return $this->repo->createQueryBuilder('t')
+			->select('COUNT(t.id)')
+			->innerJoin('t.networks', 'n')
+			->andWhere('n.networks = :networks and n.identity = :identity')
+			->setParameter(':network', $network)
+			->setParameter(':identity', $identity)
+			->getQuery()
+			->getSingleResult() > 0;
+	}
+
+    public function add(User $user): void
+    {
+    	$this->em->persist($user);
+    }
+    
 }
