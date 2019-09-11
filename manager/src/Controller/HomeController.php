435047e85f7bde021f536346f80e8e2a243f2d81
Rupa4ok <rupak4@yandex.ru>
2019-09-11 07:58:04 +0300

    Add functioanl test

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Model\User\Entity\UserRepository;
-use App\ReadModel\User\UserFetcher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param UserRepository $users
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function index(UserRepository $users)
     {
---
+++ manager/tests/Functional/HomeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+	public function testGuest(): void
+	{
+		$client = static::createClient();
+		$client->request('GET', '/');
+		
+		$this->assertSame(302, $client->getResponse()->getStatusCode());
+		$this->assertSame('http://localhost/login', $client->getResponse()->headers->get('location'));
+	}
+}
