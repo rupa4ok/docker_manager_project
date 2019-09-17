121606eb8b10466728a56455796eef37a4107693
rupa4ok <rupak4@yandex.ru>
2019-09-17 17:54:05 +0300

    Fix travis

--- manager/src/Controller/HomeController.php
+++ manager/src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\ReadModel\User\UserFetcher;
+use Elastica\Query;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +18,16 @@ class HomeController extends AbstractController
 	 */
     public function index(UserFetcher $users)
     {
+
+        $finder = $this->container->get('fos_elastica.finder.user.theme');
+        $query = new Query();
+
+        $users = $finder->find($boolQuery);
+
+        dump($finder);
+
         return $this->render('app/home.html.twig', [
-            'users' => ''
+            'users' => $users
         ]);
     }
 }
